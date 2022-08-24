a9de1019708d7034051cb433301ba1e83c40984e
caleb-freitas <calebfreitas@tutanota.com>
2022-08-24 18:23:38 -0300

    refactor: removed useless zod validation

--- src/schema/item.ts
+++ src/schema/item.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const createItemSchema = z.object({
   name: z.string(),
-  quantity: z.number().nonnegative().refine((value) => parseInt((value.toString())))
+  quantity: z.number().nonnegative()
 })
 
 export type CreateItemInput = z.TypeOf<typeof createItemSchema>
